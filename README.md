6179bb4b493a9e06155cb31f9800583372110eac
fjbo-net <fjbo@outlook.com>
2017-05-10 13:29:56 -0700

    First "clean" working prototype of strbr.
    This is a refined working prototype of strbr.
    
    List of Changes:
    * Added Documentation to README.md
    * Renamed source code to strbr.js. Cleaned source and added comments.

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# strobo.js
+# strbr.js
+
+A JavaScript Animation Frame controller that makes easy to queue, execute, manage and remove different functions for effects or animation to the window.requestAnimationFrame() method. Just like HTML 5 Canvas renderings.
+
+## I don't get it
+
+Using event handlers like Obj.addEventListener('whateverEvent', awesomeness); $(...).on('whateverEvent', awesomeness), intervals or timeouts are not as cool as they used to be. Just because of performance, nothing personal. Instead, it is highly recommended to use the window.requestAnimationFrame() method to let the browser execute your effects/animations whenever it is more convenient (and avoid executing your callback like 300 different times per event).
+
+Because the ultimate goal is a better-performing web and make life easier, strbr.js lets you spice-up your projects without a lot of struggle to keep things on the loop. strbr.js is an object that contains and manages your functions to avoid redundancy and unnecessary function execution.
+
+## Ahh, I see
+
+Pretty glad you do! Your users will appreciate it.
+
+If you want to learn more about the requestAnimationFrame() method, check out Mozilla's documentation: https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+
+# Features
+
+* Object-oriented
+* API-style model
+* Single instance
+* Event Filtering
+
+# How to use it
+
+Using strbr.js is easy: Include. Queue. Manage.
+
+## Include
+
+Add `<script src="strbr.js"></script>` to your document's <head> tag like:
+
+```html
+<!doctype html>
+<html>
+	<head>
+		<title>Page Title Here</title>
+		<!-- strbr.js -->
+		<script src="strbr.js"></script>
+		<!-- Your Cool script -->
+		<script src="awesome.js"></script>
+	</head>
+	<body>
+	</body>
+	<!-- even more awesome stuff -->
+	<script src="super-awesome.js"></script>
+</html>
+```
+
+ *NOTE:* Must be included before any script that strbr.js is a dependency of.
+
+## Queue
+
+To add functions to the strbr object just use `strbr.add('id', function, 'event');`. The add method accepts the following parameters:
+* **id** - string - (required) A string containing the function unique identifier. It can be the function name, a nickname, a lame 'function#'. It's all up to you.
+* **function** - function - (required) Function to be executed. Can be an anonymous function or the function object.
+* **event** - string - (optional) Name of the event that should trigger your function. It only executes when that event has been detected. Supported values: 'scroll', 'resize'.
+
+## Manage
+
+Sometimes you just get enough of something. Strbr.js allows you to remove functions from the execution queue or pause execution of "Event Queues" or functions individually.
+
+### Removing
+
+To remove functions from the strbr object just use `strbr.remove(id, event);`. The remove method accepts the following parameters.
+* **id** - string - (required) A string containing the function unique identifier. It can be the function name, a nickname, a lame 'function#'. It's all up to you.
+* **event** - string - (optional) Name of the event that should trigger your function. It only executes when that event has been detected. Supported values: 'scroll', 'resize'.
---
+++ strbr.js
@@ -0,0 +1,162 @@
+/**
+ * http://strbr.fjbo.net/
+ * JavaScript Animation Frame Manager
+ * @version v0.0.1
+ * @author Francisco Javier Becerra-Ortiz (FJBO)
+ * @copyright © 2017 FJBO.
+**/
+
+/**
+ * Animation Controller. Contains the API methods for strbr.
+ *
+ * @constructor
+ */
+function strbr() {
+	this.resize = {
+		active: true,
+		prevX: 0,
+		prevY: 0,
+		registered: [],
+		container: {}
+	}
+	this.scroll = {
+		active: true,
+		prevX: 0,
+		prevY: 0,
+		registered: [],
+		container: {}
+	}
+	this.default = {
+		active: true,
+		prevX: 0,
+		prevY: 0,
+		registered: [],
+		container: {}
+	}
+
+	this.paused = false;
+
+	/**
+	 * Adds a function from the execution queue (loop).
+	 *
+	 * @param {string} i - (required) The function identifier. This is the id
+	 * used for registering and removing the function from the controller's
+	 * queue.
+	 * @param {function} f - (required) The function (or callback), to be
+	 * called during the execution queue (loop).
+	 * @param {string} e - (optional) The event queue identifier. This string
+	 * tells the controller which queue the function is intended for.
+	 * Supported Values:
+	 *  'default':
+	 *  Default value if no event is provided. Executes on every frame.
+	 *  'scroll':
+	 *  Defines a function to be added to scroll loop. Executes only when scroll
+	 *  X or scroll Y values differ from previous value.
+	 *  'resize':
+	 *  Defines a function to be added to resize loop. Executes only when window
+	 *  innerHeight or innerWidth values differ from previous values.
+	 */
+	strbr.prototype.add = function(i, f, e) {
+		if(typeof(e) == 'undefined') {
+			var e = 'default';
+		}
+		switch(e.toLowerCase()) {
+			case 'resize':
+			if(!this.resize.container.hasOwnProperty(i)) this.resize.container[i] = f;
+			break;
+			case 'scroll':
+			if(!this.scroll.container.hasOwnProperty(i)) this.scroll.container[i] = f;
+			break;
+			default:
+			if(!this.default.container.hasOwnProperty(i)) this.default.container[i] = f;
+			break;
+		}
+		f();
+		return i;
+	}
+
+	/**
+	 * Removes a function from the execution queue (loop).
+	 */
+	strbr.prototype.remove = function (i, e) {
+		if(typeof(e) == 'undefined') {
+			var e = 'default';
+		}
+		switch(e.toLowerCase()) {
+			case 'resize':
+			if(this.resize.container.hasOwnProperty(i)) delete this.resize.container[i];
+			break;
+			case 'scroll':
+			if(this.scroll.container.hasOwnProperty(i)) delete this.scroll.container[i];
+			break;
+			default:
+			if(this.default.container.hasOwnProperty(i)) delete this.default.container[i];
+			break;
+		}
+	}
+
+	/**
+	 * Toggles strbr pause state.
+	 */
+	strbr.prototype.pause = function () {
+		if(this.paused == true) {
+			this.paused = false;
+		} else {
+			this.paused = true;
+		}
+	}
+
+	/**
+	 * This method is the execution queue that contains all the event queues.
+	 */
+	strbr.prototype.run = function() {
+		if(this.paused != true) {
+			if(this.default.active == true) {
+				for(var key in this.default.container) {
+					if(this.default.container.hasOwnProperty(key)) {
+						this.default.container[key]();
+					}
+				}
+			}
+			if(this.scroll.active == true) {
+				if(window.scrollY != this.scroll.prevY || window.scrollX != this.scroll.prevX) {
+					this.scroll.prevX = window.scrollX;
+					this.scroll.prevY = window.scrllY;
+
+					for(var key in this.scroll.container) {
+						if(this.scroll.container.hasOwnProperty(key)) {
+							this.scroll.container[key]();
+						}
+					}
+				}
+			}
+			if(this.resize.active == true) {
+				if(window.scrollY != this.resize.prevY || window.scrollX != this.resize.prevX) {
+					this.resize.prevX = window.innerWidth;
+					this.resize.prevY = window.innerHeight;
+
+					for(var key in this.resize.container) {
+						if(this.resize.container.hasOwnProperty(key)) {
+							this.resize.container[key]();
+						}
+					}
+				}
+			}
+		}
+		this.frame = requestAnimationFrame(this.run.bind(this));
+	}
+
+	/**
+	 * This function stops the execution queue. To reactivate strbr, run strbr.run();
+	 */
+	this.stop = function() {
+		cancelAnimationFrame(this.frame);
+		return;
+	}
+	this.frame = requestAnimationFrame(this.run.bind(this));
+}
+
+/*
+ * Auto-initialize strbr. Creates an instance at the window object.
+ */
+(function(){ window.strbr = new strbr();})();
